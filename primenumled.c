5003115f262bdf09af57904d9586a27e03cb5540
RyoheiGoto <s1526051yc@s.chibakoudai.jp>
2017-12-29 19:13:44 +0900

    Add src files

---
+++ primenumled.c
@@ -0,0 +1,118 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/uaccess.h>
+#include <linux/io.h>
+
+#define LED_PIN 25
+
+MODULE_AUTHOR("Ryohei Goto");
+MODULE_DESCRIPTION("identify prime numbers [1-9]");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+
+static dev_t dev;
+static struct cdev cdv;
+static struct class *cls = NULL;
+
+static volatile u32 *gpio_base = NULL;
+
+int is_prime_number(const int n)
+{
+	int i;
+
+	if(n == 2){
+		return 1;
+	}
+	else if((n < 2) || ((n % 2) == 0)){
+		return 0;
+	}
+
+	for(i = 3; i < n; i += 2){
+		if((n % i) == 0){
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
+{
+	char c;
+	int ret;
+
+	if(copy_from_user(&c, buf, sizeof(char))){
+		return -EFAULT;
+	}
+
+	if((c >= '0') && (c <= '9')){
+		ret = is_prime_number((int)(c - '0'));
+
+		if(ret){
+			gpio_base[7] = 1 << LED_PIN;
+		}
+		else{
+			gpio_base[10] = 1 << LED_PIN;
+		}
+	}
+
+	return 1;
+}
+
+static struct file_operations led_fops = {
+	.owner = THIS_MODULE,
+	.write = led_write
+};
+
+static int __init init_mod(void)
+{
+	int retval;
+
+	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+
+	const u32 led = LED_PIN;
+	const u32 index = led / 10;
+	const u32 shift = (led % 10) * 3;
+	const u32 mask = ~(0x7 << shift);
+	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+
+	retval =  alloc_chrdev_region(&dev, 0, 1, "primenumled");
+	if(retval < 0){
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
+	}
+	printk(KERN_INFO "%s is loaded. major:%d\n", __FILE__, MAJOR(dev));
+
+	cdev_init(&cdv, &led_fops);
+	cdv.owner = THIS_MODULE;
+	retval = cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));
+		return retval;
+	}
+
+	cls = class_create(THIS_MODULE, "primenumled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed.");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "primenumled%d", MINOR(dev));
+
+	return 0;
+}
+
+static void __exit cleanup_mod(void)
+{
+	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__, MAJOR(dev));
+	iounmap(gpio_base);
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
+
